06a0a90b4ea4d7024d052fd49dfcb63f9b46329e
Nguyen Van Chinh <chinhnv.uet@gmail.com>
2024-01-11 13:52:14 +0700

    fix crawlCode can adapt with link googleads 2

--- dags/mainDag.py
+++ dags/mainDag.py
@@ -3,21 +3,21 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-# from operators.crawlData import crawData
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from python.crawlWebData import executeCrawl
 from python.transformData import transform
 from python.loadToDB import loadToMySql
+
 # from airflow_pentaho.operators.KitchenOperator import KitchenOperator
 
 with DAG(
-    dag_id="crawlDataJob",
+    dag_id="crawlDataGoogleadsJob",
     start_date=datetime.datetime(2021, 1, 1),
     schedule_interval=timedelta(days=2000),
-    tags=["etl", "v1", "googleads"],
+    tags=["etl", "pentaho", "googleads"],
 ) as dag:
-    crawlData = PythonOperator(task_id='crawl_data', python_callable=executeCrawl)
-    transform = PythonOperator(task_id='transform_data', python_callable=transform)
-    loadData = PythonOperator(task_id='load_data_toMysql', python_callable=loadToMySql)
-    crawlData >> transform >> loadData
+    crawlData = PythonOperator(task_id="crawl_data", python_callable=executeCrawl)
+    transformData = PythonOperator(task_id="transform_data", python_callable=transform)
+    loadData = PythonOperator(task_id="load_data_toMysql", python_callable=loadToMySql)
+    crawlData >> transformData >> loadData
